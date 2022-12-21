72bbfa0602ea7c3789ce797958aa8e1677cf6c63
Conner Laursen <conner@Conners-MacBook-Air.local>
2022-12-20 18:11:29 -0800

    added days 2 and 3

---
+++ 12-Stu_Parameter-Check/index.js
@@ -0,0 +1,16 @@
+
+let argument1="";
+let argument2="";
+
+argument1 = process.argv[2];
+argument2 = process.argv[3];
+
+if(argument1 === argument2){
+
+    console.log("they are equal");
+}else{
+
+    console.log ("they are not the same");
+}
+
+
